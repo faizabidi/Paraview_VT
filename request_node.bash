9f1cba8d3912c2f9c1d80a09d2fa5a32a17e9b69
faizabidi <fabidi89@vt.edu>
2017-05-25 11:54:34 -0400

    Request a node on Newriver.

---
+++ request_node.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Run this script from the login node on NewRiver."
+
+if [ $# -ne 3 ]; then
+		echo "Enter walltime, # nodes, and email address."
+		echo "Example: ./request_node.bash 4 1 fabidi89@vt.edu"
+		exit 1
+fi
+
+WALLTIME=$1
+NODES=$2
+EMAIL=$3
+
+qsub -I -l walltime=$1:00:00 -l nodes=$2:ppn=24:gpus=2 -q vis_q -W
+group_list=newriver -A arctest -M $3 -m bea
