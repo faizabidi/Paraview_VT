e2aff542019f2df9f1f00e03aa675e85b72bc39b
faizabidi <fabidi89@vt.edu>
2017-05-25 15:56:45 -0400

    Start ParaView servers on the NewRiver login node.

---
+++ start_paraview_server.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This assumes that you have ParaView 5.2.0 installled somewhere.
+# In this file, I have hardcoded this to the path in my home directory. 
+
+if [ $# -ne 1 ]; then
+	echo "Enter # of pvservers to start."
+	echo "Example: start_pvserver.bash 8"
+	exit 1
+fi
+
+# Kill any existing running pvservers
+ps -ef | grep pvserver | grep -v grep | awk '{print $2}' | xargs kill  
+
+if [ $# -ne 1 ]; then
+	echo "Enter # of pvservers to start."
+	echo "Example: start_pvserver.bash 8"
+fi
+
+SERVER=$1 
+
+# Start new pvservers
+mpiexec -np $1 \
+	vglrun /home/fabidi89/Paraview-v5.2.0/bin/pvserver \
+		/home/fabidi89/git/Thesis/vnc_operations/cave-momo.pvx &
+
